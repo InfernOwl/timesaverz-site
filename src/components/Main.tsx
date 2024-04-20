0ab1fb756e854f1335ff7842013cfbf62b8751bd
InfernOwl <cchampagnie.1991@gmail.com>
2024-04-20 15:50:36 -0400

    Adding new suggestion form submit functions and schema models

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,10 +2,47 @@ import React from "react";
 import { Image, Form, Input, Button } from "antd";
 import '../styles/Home.css';
 import aboutJSON from '../constants/about.json';
+import { Suggestion } from "../models";
+import { DataStore } from "aws-amplify/datastore";
+import { useState } from "react";
+
 
 const { TextArea } = Input;
 
+const onFinish = () => {
+
+}
+
+const onSubmit = (suggest: String) => {
+    try {
+        const post = DataStore.save(
+          new Suggestion({
+            suggestion: `${suggest}`
+          })
+        );
+        console.log('Post saved successfully!', post);
+      } catch (error) {
+        console.log('Error saving post', error);
+      }
+}
+
 const Main = () => {
+
+    const [state, setState] = useState({
+        suggestion: "",
+    })
+
+    const changeState = (e:any) => {
+        const {value} = e.target;
+        console.log(value);
+        setState({
+            suggestion: value
+        })
+    }
+
+    // TODO: Succesfully update remote graphql database
+    //          Add input field clear onFinish of form
+    //          Add alerts for successfull data entry and failed data entry on finish
 
     return (
         <>
@@ -14,7 +51,7 @@ const Main = () => {
                 <h1>RACE NIGHT!</h1>
 
                 <p>Welcome to <strong>"Race Nights"</strong> a seasonal speedrun format where our three streamers (<a href="https://www.twitch.tv/hoagiepops" target="_blank">Hoagiepops</a>, <a href="https://www.twitch.tv/infernowl" target="_blank">InfernOwl</a>, and <a href="https://www.twitch.tv/hoagiepops" target="_blank">LSJay</a>) learn, run, and RACE some amazing games over the course of 3 months doing their best to beat times, earn points, and gain bragging rights!</p>
-                <p>Each season our runners have 3 attempts to best their fellow racers as well as their own times and earn points for the leaderboard. Races happen on the last Thursday of each month but practice happens for the entire 3 month period, so our runners are constantly improving and making the competition as close as possible.</p>
+                <p>Each season our runners have 3 attempts0 to best their fellow racers as well as their own times and earn points for the leaderboard. Races happen on the last Thursday of each month but practice happens for the entire 3 month period, so our runners are constantly improving and making the competition as close as possible.</p>
                 <p>At the end of the season, the runner with the fastest time on the leaderboard gets bonus points, and the runner with the most points is declared the winner! The runner with the lowest total points is, of course, the loser and has to do a punishment run.</p>
                 <p>The goal of The Timesaverz is to have fun speedrunning, showcase fun speedgames that might not get as much love and activity as they deserve, and work to grow the community of speedrunning overall!</p>
                 <p>So whether you're just watching the <a href="/races">Races</a>, or want to run alongside us in our <a href="/sweepstakes">Sweepstakes</a> we hope that you enjoy the runs!</p>
@@ -24,7 +61,7 @@ const Main = () => {
                     <h2>Suggestion Box!</h2>
                     <p>Have a game suggestion for Race Nights? Maybe a game you'd like us to run, schedule suggestions, visual thoughts?<br />
                         Submit them below and it'll send them straight to us!<br />
-                        We appreciate any and all suggestions!</p>
+                        We appreciate any and alla suggestions!</p>
                     <Form
                       labelCol={{offset: 2}}
                       labelAlign="left"
@@ -33,10 +70,10 @@ const Main = () => {
                       style={{ margin: "auto", paddingTop: '5%', width: '90%', color: 'white'}}
                       initialValues={{remember: true}}>
                         <Form.Item className="formLabel">
-                            <TextArea rows={4} />
+                            <TextArea rows={4} name="suggestionBox" value={state.suggestion} onChange={changeState}/>
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" name="Submit">Submit</Button>
+                            <Button type="primary" name="submit" onClick={() => onSubmit(state.suggestion)} htmlType="submit">Submit</Button>
                         </Form.Item>
                     </Form>
 
