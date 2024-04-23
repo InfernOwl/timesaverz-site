ffb3dd80b58aada54099df32c5f87b4a62f9f3de
InfernOwl <cchampagnie.1991@gmail.com>
2024-04-23 17:04:41 -0400

    Reinstalling GraphQL with the proper AppID

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "timesaverzsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
